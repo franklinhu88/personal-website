10f3d2fc0f1962a07f63bd3030c18d884de582c3
franklinhu88 <franklinhu88@gmail.com>
2025-12-26 22:18:40 -0500

    scroll only works on left side now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,21 @@ export default function Home() {
     <>
       <Header />
 
-      <main className="pt-20 mx-auto max-w-7xl px-6">
-        <div className="grid grid-cols-12 gap-6">
+      <main className="pt-20 mx-auto max-w-7xl px-6 h-[calc(100vh-5rem)]">
+        <div className="grid grid-cols-12 gap-6 h-full">
           {/* Left: Profile */}
-          <section id="home" className="col-span-8">
+          <section
+            id="home"
+            className="col-span-8 overflow-auto h-full scrollbar-hide"
+          >
             <ProfileMain />
           </section>
 
-          {/* Right: Status */}
-          <aside id="activity" className="col-span-4">
+          {/* Right: Status / Sidebar */}
+          <aside
+            id="activity"
+            className="col-span-4 sticky top-20 h-[calc(100vh-5rem)] overflow-hidden"
+          >
             <ProfileSidebar />
           </aside>
         </div>
